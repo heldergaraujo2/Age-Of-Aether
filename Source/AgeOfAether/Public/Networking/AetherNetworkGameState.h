d43593c68d082f3ecb728ea85b73517c9d8381f5
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-19 10:30:44 -0300

    feat(multiplayer): replicate server admission state

--- Source/AgeOfAether/Public/Networking/AetherNetworkGameState.h
+++ Source/AgeOfAether/Public/Networking/AetherNetworkGameState.h
@@ -21,8 +21,11 @@ public:
 
     UFUNCTION(BlueprintPure, Category = "Age of Aether|Networking")
     uint64 GetAuthoritativeStateRevision() const;
-
+    UFUNCTION(BlueprintPure, Category = "Age of Aether|Networking") int32 GetConnectedPlayerCount() const { return ConnectedPlayerCount; }
+    UFUNCTION(BlueprintPure, Category = "Age of Aether|Networking") bool IsAcceptingConnections() const { return bAcceptingConnections; }
     void AdvanceAuthoritativeState();
+    void SetConnectedPlayerCount(int32 Count);
+    void SetAcceptingConnections(bool bAccepting);
 
 protected:
     UPROPERTY(Replicated, BlueprintReadOnly, Category = "Age of Aether|Networking")
@@ -30,4 +33,10 @@ protected:
 
     UPROPERTY(Replicated, BlueprintReadOnly, Category = "Age of Aether|Networking")
     uint64 AuthoritativeStateRevision = 0;
+
+    UPROPERTY(Replicated, BlueprintReadOnly, Category = "Age of Aether|Networking")
+    int32 ConnectedPlayerCount = 0;
+
+    UPROPERTY(Replicated, BlueprintReadOnly, Category = "Age of Aether|Networking")
+    bool bAcceptingConnections = true;
 };
