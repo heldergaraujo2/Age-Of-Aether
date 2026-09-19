163da69389a20d28594c78c10de84fe7d3f09ed3
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-18 21:03:45 -0300

    feat(network): add network game mode defaults

---
+++ Source/AgeOfAether/Public/Networking/AetherNetworkGameMode.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+
+#include "AetherNetworkGameMode.generated.h"
+
+UCLASS()
+class AGEOFAETHER_API AAetherNetworkGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    AAetherNetworkGameMode();
+};
