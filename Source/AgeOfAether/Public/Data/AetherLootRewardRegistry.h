2f9d61fc1137440c1301514fccec2906de5613cb
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-19 17:07:19 -0300

    feat: complete phase 24 registry

--- Source/AgeOfAether/Public/Data/AetherLootRewardRegistry.h
+++ Source/AgeOfAether/Public/Data/AetherLootRewardRegistry.h
@@ -13,10 +13,14 @@ public:
     bool RegisterLootTable(const FAetherLootTableDefinition& Definition, FString& OutError);
     bool RegisterReward(const FAetherRewardDefinition& Definition, FString& OutError);
     bool RegisterRespawn(const FAetherRespawnDefinition& Definition, FString& OutError);
+    bool RegisterDropRule(const FAetherDropRuleDefinition& Definition, FString& OutError);
+    bool RegisterSpawnGroup(const FAetherSpawnGroupDefinition& Definition, FString& OutError);
 
     bool ResolveLootTable(const FString& ID, FAetherLootTableDefinition& OutDefinition) const;
     bool ResolveReward(const FString& ID, FAetherRewardDefinition& OutDefinition) const;
     bool ResolveRespawn(const FString& ID, FAetherRespawnDefinition& OutDefinition) const;
+    bool ResolveDropRule(const FString& ID, FAetherDropRuleDefinition& OutDefinition) const;
+    bool ResolveSpawnGroup(const FString& ID, FAetherSpawnGroupDefinition& OutDefinition) const;
     bool Contains(const FString& ID) const;
 
     bool Validate(TArray<FAetherLootRewardValidationIssue>& OutIssues,
@@ -29,12 +33,16 @@ public:
     int32 NumLootTables() const { return LootTables.Num(); }
     int32 NumRewards() const { return Rewards.Num(); }
     int32 NumRespawns() const { return Respawns.Num(); }
+    int32 NumDropRules() const { return DropRules.Num(); }
+    int32 NumSpawnGroups() const { return SpawnGroups.Num(); }
     void Reset();
 
 private:
     TMap<FString, FAetherLootTableDefinition> LootTables;
     TMap<FString, FAetherRewardDefinition> Rewards;
     TMap<FString, FAetherRespawnDefinition> Respawns;
+    TMap<FString, FAetherDropRuleDefinition> DropRules;
+    TMap<FString, FAetherSpawnGroupDefinition> SpawnGroups;
 
     bool IsIDAvailable(const FString& ID) const;
     void AddIssue(TArray<FAetherLootRewardValidationIssue>& OutIssues,
