af0ba1e0359b6350efbab4833823759479df0e7e
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-18 21:03:38 -0300

    feat(network): implement replicated authoritative state

---
+++ Source/AgeOfAether/Private/Networking/AetherNetworkGameState.cpp
@@ -0,0 +1,38 @@
+#include "Networking/AetherNetworkGameState.h"
+
+#include "Net/UnrealNetwork.h"
+
+AAetherNetworkGameState::AAetherNetworkGameState()
+{
+    bReplicates = true;
+    ProtocolVersion.Major = 1;
+    ProtocolVersion.Minor = 0;
+}
+
+void AAetherNetworkGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(AAetherNetworkGameState, ProtocolVersion);
+    DOREPLIFETIME(AAetherNetworkGameState, AuthoritativeStateRevision);
+}
+
+FAetherProtocolVersion AAetherNetworkGameState::GetProtocolVersion() const
+{
+    return ProtocolVersion;
+}
+
+uint64 AAetherNetworkGameState::GetAuthoritativeStateRevision() const
+{
+    return AuthoritativeStateRevision;
+}
+
+void AAetherNetworkGameState::AdvanceAuthoritativeState()
+{
+    if (!HasAuthority())
+    {
+        return;
+    }
+
+    ++AuthoritativeStateRevision;
+}
