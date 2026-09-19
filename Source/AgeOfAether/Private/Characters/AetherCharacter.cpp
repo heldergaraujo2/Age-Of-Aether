1ffb0d33aebb21e98541248dbb14b46a66c25600
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-18 21:17:41 -0300

    feat(character): add replicated character framework

---
+++ Source/AgeOfAether/Private/Characters/AetherCharacter.cpp
@@ -0,0 +1,68 @@
+#include "Characters/AetherCharacter.h"
+
+#include "Characters/AetherCharacterPlayerState.h"
+#include "Characters/AetherCharacterSubsystem.h"
+#include "Networking/AetherNetworkPlayerController.h"
+
+#include "Net/UnrealNetwork.h"
+
+AAetherCharacter::AAetherCharacter()
+{
+    bReplicates = true;
+    SetReplicateMovement(true);
+}
+
+void AAetherCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+    DOREPLIFETIME(AAetherCharacter, CharacterId);
+}
+
+void AAetherCharacter::PossessedBy(AController* NewController)
+{
+    Super::PossessedBy(NewController);
+
+    if (!HasAuthority())
+    {
+        return;
+    }
+
+    AAetherCharacterPlayerState* CharacterState = GetPlayerState<AAetherCharacterPlayerState>();
+    AAetherNetworkPlayerController* CharacterController = Cast<AAetherNetworkPlayerController>(NewController);
+
+    if (CharacterState && CharacterController && CharacterController->IsAccountAuthenticated())
+    {
+        CharacterId = CharacterState->GetCharacterId();
+    }
+}
+
+void AAetherCharacter::UnPossessed()
+{
+    if (HasAuthority())
+    {
+        if (AAetherCharacterPlayerState* CharacterState = GetPlayerState<AAetherCharacterPlayerState>())
+        {
+            if (AAetherNetworkPlayerController* Controller = Cast<AAetherNetworkPlayerController>(GetController()))
+            {
+                if (Controller->IsAccountAuthenticated())
+                {
+                    if (UAetherCharacterSubsystem* Characters = GetGameInstance()
+                        ? GetGameInstance()->GetSubsystem<UAetherCharacterSubsystem>()
+                        : nullptr)
+                    {
+                        Characters->DeselectCharacter(
+                            Controller->GetAuthenticatedAccountId(),
+                            CharacterState->GetCharacterId());
+                    }
+                }
+            }
+        }
+    }
+
+    Super::UnPossessed();
+}
+
+FAetherCharacterId AAetherCharacter::GetCharacterId() const
+{
+    return CharacterId;
+}
