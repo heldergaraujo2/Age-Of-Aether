8f5c4f5ba0a859e4f0dcdb4326e5511a9074b38b
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-19 10:43:32 -0300

    feat(persistence): expose restore boundaries for runtime state

--- Source/AgeOfAether/Public/Items/AetherItemService.h
+++ Source/AgeOfAether/Public/Items/AetherItemService.h
@@ -12,6 +12,7 @@ public:
     bool FindDefinition(const FAetherItemDefinitionId& DefinitionId, FAetherItemDefinition& OutDefinition) const;
 
     bool GetInventory(const FAetherCharacterId& CharacterId, TArray<FAetherInventorySlot>& OutSlots) const;
+    bool RestoreInventory(const FAetherCharacterId& CharacterId, const TArray<FAetherInventorySlot>& Slots);
 
     bool AddItem(const FAetherCharacterId& CharacterId, const FAetherItemDefinitionId& DefinitionId, int32 Quantity, TArray<FAetherInventorySlot>& OutInventory);
     bool RemoveItem(const FAetherCharacterId& CharacterId, const FAetherItemInstanceId& InstanceId, int32 Quantity, TArray<FAetherInventorySlot>& OutInventory);
