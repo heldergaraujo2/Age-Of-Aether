88401e1ccf26f4f71902da50ff12e295111d7535
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-19 10:19:10 -0300

    fix(items): expose service for transactional economy integration

--- Source/AgeOfAether/Public/Items/AetherItemSubsystem.h
+++ Source/AgeOfAether/Public/Items/AetherItemSubsystem.h
@@ -12,7 +12,8 @@ class AGEOFAETHER_API UAetherItemSubsystem : public UGameInstanceSubsystem
     GENERATED_BODY()
 
 public:
-    bool RegisterDefinition(const FAetherItemDefinition& Definition);\n    bool RegisterDefinitionAsset(const UAetherItemDefinitionDataAsset* DefinitionAsset);
+    bool RegisterDefinition(const FAetherItemDefinition& Definition);
+    bool RegisterDefinitionAsset(const UAetherItemDefinitionDataAsset* DefinitionAsset);
     bool FindDefinition(const FAetherItemDefinitionId& DefinitionId, FAetherItemDefinition& OutDefinition) const;
     bool GetInventory(const FAetherCharacterId& CharacterId, TArray<FAetherInventorySlot>& OutInventory) const;
 
@@ -21,6 +22,8 @@ public:
     bool MoveItem(const FAetherCharacterId& CharacterId, const FAetherItemInstanceId& InstanceId, int32 TargetSlot, TArray<FAetherInventorySlot>& OutInventory);
     bool SplitStack(const FAetherCharacterId& CharacterId, const FAetherItemInstanceId& InstanceId, int32 Quantity, int32 TargetSlot, TArray<FAetherInventorySlot>& OutInventory);
     bool MergeStacks(const FAetherCharacterId& CharacterId, const FAetherItemInstanceId& SourceInstanceId, const FAetherItemInstanceId& TargetInstanceId, TArray<FAetherInventorySlot>& OutInventory);
+    FAetherItemService& GetItemService() { return ItemService; }
+    const FAetherItemService& GetItemService() const { return ItemService; }
 
 private:
     FAetherItemService ItemService;
