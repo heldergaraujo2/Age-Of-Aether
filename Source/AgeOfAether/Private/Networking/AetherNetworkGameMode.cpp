3babaab96457f0b0df6a29570b221dda659c42b3
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-18 21:18:18 -0300

    feat(character): configure authoritative character pawn and player state

--- Source/AgeOfAether/Private/Networking/AetherNetworkGameMode.cpp
+++ Source/AgeOfAether/Private/Networking/AetherNetworkGameMode.cpp
@@ -2,9 +2,47 @@
 
 #include "Networking/AetherNetworkGameState.h"
 #include "Networking/AetherNetworkPlayerController.h"
+#include "Characters/AetherCharacter.h"
+#include "Characters/AetherCharacterPlayerState.h"
+#include "GameFramework/PlayerStart.h"
 
 AAetherNetworkGameMode::AAetherNetworkGameMode()
 {
     GameStateClass = AAetherNetworkGameState::StaticClass();
     PlayerControllerClass = AAetherNetworkPlayerController::StaticClass();
+    PlayerStateClass = AAetherCharacterPlayerState::StaticClass();
+    DefaultPawnClass = AAetherCharacter::StaticClass();
+    bStartPlayersAsSpectators = true;
+}
+
+
+void AAetherNetworkGameMode::SpawnSelectedCharacter(
+    APlayerController* PlayerController,
+    const FAetherCharacterRecord& Character)
+{
+    if (!HasAuthority() || !PlayerController)
+    {
+        return;
+    }
+
+    if (PlayerController->GetPawn())
+    {
+        PlayerController->GetPawn()->Destroy();
+    }
+
+    RestartPlayer(PlayerController);
+
+    AAetherCharacter* SpawnedCharacter = Cast<AAetherCharacter>(PlayerController->GetPawn());
+    if (!SpawnedCharacter)
+    {
+        return;
+    }
+
+    if (AAetherCharacterPlayerState* CharacterState = PlayerController->GetPlayerState<AAetherCharacterPlayerState>())
+    {
+        CharacterState->SetCharacterIdentity(Character);
+    }
+
+    SpawnedCharacter->SetActorLocation(Character.WorldLocation);
+    SpawnedCharacter->SetActorRotation(Character.WorldRotation);
 }
