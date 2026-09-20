28506906af6d5794f22c07e3c845400c957aec52
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-20 06:43:30 -0300

    Phase 37: wire visual foundation GameMode

--- Source/AgeOfAether/Public/Networking/AetherNetworkGameMode.h
+++ Source/AgeOfAether/Public/Networking/AetherNetworkGameMode.h
@@ -13,6 +13,7 @@ class AGEOFAETHER_API AAetherNetworkGameMode : public AGameModeBase
 
 public:
     AAetherNetworkGameMode();
+    virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
     virtual void PostLogin(APlayerController* NewPlayer) override;
     virtual void Logout(AController* Exiting) override;
 
