0c48917e9bfae3d6d7a21ea82fa9906207405b7e
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-20 05:50:21 -0300

    Phase 35: add generated header for simulation USTRUCTs

--- Source/AgeOfAether/Public/Data/AetherClassBalanceSimulation.h
+++ Source/AgeOfAether/Public/Data/AetherClassBalanceSimulation.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "CoreMinimal.h"
 #include "Data/AetherClassBalanceRegistry.h"
+#include "AetherClassBalanceSimulation.generated.h"
 
 USTRUCT(BlueprintType)
 struct FAetherBalanceSimulationCase
