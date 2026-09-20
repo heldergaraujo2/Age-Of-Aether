02f4b10f4541f7605003ec4b7cddc50c1e9fbd7f
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-20 06:09:27 -0300

    Phase 36: add class presentation catalog

---
+++ Source/AgeOfAether/Public/Presentation/AetherClassPresentationCatalog.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Presentation/AetherClassPresentationTypes.h"
+class FAetherClassPresentationCatalog{public:static void Build(TArray<FAetherClassPresentationDefinition>& OutDefinitions);};
