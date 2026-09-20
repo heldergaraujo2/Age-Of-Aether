fe3e7bf750ed30fa620f10fdcf653c0b59eaf043
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-20 06:41:38 -0300

    Phase 37: add Unreal visual foundation AetherGameInstance.h

---
+++ Source/AgeOfAether/Public/Core/AetherGameInstance.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+
+#include "AetherGameInstance.generated.h"
+
+UCLASS()
+class AGEOFAETHER_API UAetherGameInstance : public UGameInstance
+{
+    GENERATED_BODY()
+
+public:
+    UAetherGameInstance();
+
+    UFUNCTION(BlueprintPure, Category = "Age of Aether|Foundation")
+    static UAetherGameInstance* GetAether(const UObject* WorldContextObject);
+
+    UFUNCTION(BlueprintPure, Category = "Age of Aether|Foundation")
+    FName GetDevelopmentMapId() const;
+
+    UFUNCTION(BlueprintPure, Category = "Age of Aether|Foundation")
+    int32 GetVisualFoundationVersion() const;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Config, Category = "Age of Aether|Foundation")
+    FName DevelopmentMapId = TEXT("aether_development");
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Config, Category = "Age of Aether|Foundation")
+    int32 VisualFoundationVersion = 1;
+};
