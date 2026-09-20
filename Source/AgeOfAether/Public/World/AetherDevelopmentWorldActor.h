4fabe84b735c766f3560eb2c201a98af5e2ec5f3
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-20 06:41:55 -0300

    Phase 37: add Unreal visual foundation AetherDevelopmentWorldActor.h

---
+++ Source/AgeOfAether/Public/World/AetherDevelopmentWorldActor.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "AetherDevelopmentWorldActor.generated.h"
+
+class UStaticMeshComponent;
+
+UCLASS()
+class AGEOFAETHER_API AAetherDevelopmentWorldActor : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AAetherDevelopmentWorldActor();
+
+    virtual void OnConstruction(const FTransform& Transform) override;
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Age of Aether|Foundation")
+    TObjectPtr<USceneComponent> Root;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Age of Aether|Foundation")
+    TObjectPtr<UStaticMeshComponent> Ground;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Age of Aether|Foundation")
+    FVector GroundScale = FVector(25.0, 25.0, 0.25);
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Age of Aether|Foundation")
+    float GroundZ = 0.0f;
+
+private:
+    void ConfigureGround();
+};
