6bd9ae398e582a66dc7a4b327fece650029fabb4
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-21 09:57:50 -0300

    build: sync V7 game target

--- Source/AgeOfAether.Target.cs
+++ Source/AgeOfAether.Target.cs
@@ -5,7 +5,7 @@ public class AgeOfAetherTarget : TargetRules
     public AgeOfAetherTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Game;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
+        DefaultBuildSettings = BuildSettingsVersion.V7;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("AgeOfAether");
     }
