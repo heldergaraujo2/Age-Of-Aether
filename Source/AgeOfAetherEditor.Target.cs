60cb65ac6cbafba389e1c14b9fe4c2e0682c7bf0
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-21 09:57:52 -0300

    build: sync V7 editor target

--- Source/AgeOfAetherEditor.Target.cs
+++ Source/AgeOfAetherEditor.Target.cs
@@ -5,7 +5,7 @@ public class AgeOfAetherEditorTarget : TargetRules
     public AgeOfAetherEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
+        DefaultBuildSettings = BuildSettingsVersion.V7;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("AgeOfAether");
     }
